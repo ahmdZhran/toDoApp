f3ae801075e898176820525e8f6b93d19e47459c
ahmed <ahmedmohameddx80@gmail.com>
2023-05-06 07:55:13 -0700

    float action button

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,76 +1,20 @@
-// import 'package:flutter/material.dart';
-
-// class TaskManager extends StatelessWidget {
-//   const TaskManager({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//         backgroundColor: Colors.teal[300],
-//         body: Padding(
-//           padding: const EdgeInsets.all(20),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Row(
-//                 children: [
-//                   IconButton(
-//                     onPressed: () {},
-//                     icon: const Icon(
-//                       Icons.playlist_add_check,
-//                       size: 40,
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                   const SizedBox(
-//                     width: 20,
-//                   ),
-//                   const Text(
-//                     'To DayDo',
-//                     style: TextStyle(
-//                         fontSize: 30,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.white),
-//                   ),
-//                 ],
-//               ),
-//               const Padding(
-//                 padding: EdgeInsets.only(top: 8.0, left: 12),
-//                 child: Text(
-//                   '3 Tasks',
-//                   style: TextStyle(color: Colors.white),
-//                 ),
-//               ),
-//               const SizedBox(
-//                 height: 20,
-//               ),
-//               Expanded(
-//                 child: Container(
-//                   decoration: BoxDecoration(
-//                       borderRadius: BorderRadius.circular(16),
-//                       color: Colors.white),
-//                 ),
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class TaskManager extends StatelessWidget {
   const TaskManager({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Colors.teal[300],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.teal[400],
+        child: const Icon(Icons.add),
+      ),
+      backgroundColor: Colors.teal[400],
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(screenWidth * 0.05),
