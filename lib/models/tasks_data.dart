dbd04afdacb968a4ac9b1886fc6707884c9247d4
ahmed <ahmedmohameddx80@gmail.com>
2023-05-21 15:38:38 -0700

    create function in method to add new tasks with pr

--- lib/models/tasks_data.dart
+++ lib/models/tasks_data.dart
@@ -3,4 +3,8 @@ import 'package:task_manager/models/tasks.dart';
 
 class TasksData extends ChangeNotifier {
   List<Task> tasks = [];
+  void addTask(String newTaskTitle) {
+    tasks.add(Task(name: newTaskTitle));
+    notifyListeners();
+  }
 }
