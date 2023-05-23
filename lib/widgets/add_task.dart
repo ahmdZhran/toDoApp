bcaca4d5e42d9d7cb6ea18cb452ccbc969455d4a
ahmed <ahmedmohameddx80@gmail.com>
2023-05-23 14:30:56 -0700

    add hive package

--- lib/widgets/add_task.dart
+++ lib/widgets/add_task.dart
@@ -1,61 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:task_manager/models/tasks_data.dart';
-
-// class AddTasks extends StatelessWidget {
-//   final Function addTaskCallBack;
-//   AddTasks(this.addTaskCallBack, {super.key});
-//   String? newTaskTitle;
-//   @override
-//   Widget build(BuildContext context) {
-//     String? newTaskTitle;
-//     return SizedBox(
-//       child: Padding(
-//         padding: const EdgeInsets.all(20),
-//         child: Column(children: [
-//           Text(
-//             'Add Your Task',
-//             style: TextStyle(
-//                 fontSize: 30,
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.indigo[300]),
-//           ),
-//           TextFormField(
-//               decoration: const InputDecoration(
-//                   enabledBorder: UnderlineInputBorder(
-//                       borderSide: BorderSide(color: Colors.red))),
-//               cursorColor: Colors.teal[300],
-//               autofocus: true,
-//               textAlign: TextAlign.center,
-//               onChanged: (newTask) {
-//                 newTaskTitle = newTask;
-//               },
-//           )
-//           const SizedBox(
-//             height: 20,
-//           ),
-//           Container(
-//             width: double.infinity,
-//             decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(16),
-//                 color: Colors.teal[400]),
-//             child: TextButton(
-//               onPressed: () {
-//                 Provider.of<TasksData>(context, listen: false)
-//                     .addTask(newTaskTitle!);
-//                 Navigator.pop(context);
-//               },
-//               child: const Text(
-//                 'ADD',
-//                 style: TextStyle(color: Colors.white),
-//               ),
-//             ),
-//           )
-//         ]),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:task_manager/models/tasks_data.dart';
@@ -69,7 +11,7 @@ class AddTasks extends StatelessWidget {
 
   String? validateInput(String? value) {
     if (value == null || value.isEmpty) {
-      return 'Please enter a task title';
+      return 'Enter your task';
     }
 
     return null;
