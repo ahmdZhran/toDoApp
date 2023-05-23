0b2e92ffabf9db23ecb8e5269e5f70c061cbee8e
ahmed <ahmedmohameddx80@gmail.com>
2023-05-23 16:15:48 -0700

    add initFlutter and open Box to save data

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/models/tasks_data.dart';
 import 'package:task_manager/screens/home_page.dart';
 import 'package:provider/provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'constants.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
