9ef112251aed7f70469455e8c7198ff5956ee17d
ahmed <ahmedmohameddx80@gmail.com>
2023-05-23 15:45:11 -0700

    generate code

--- lib/models/tasks.dart
+++ lib/models/tasks.dart
@@ -1,5 +1,11 @@
-class Task {
+import 'package:hive/hive.dart';
+part 'tasks.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  @HiveField(0)
   final String name;
+  @HiveField(1)
   bool isDone;
   Task({required this.name, this.isDone = false});
   void taskDone() {
